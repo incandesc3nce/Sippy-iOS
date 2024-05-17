33b25ea5ce69afd729cf866a2625b88d25eb9fbb
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-05-17 23:44:36 +0700

    Add registration and login views

--- Sippy/Sippy/Views/Login/LoginView.swift
+++ Sippy/Sippy/Views/Login/LoginView.swift
@@ -7,51 +7,91 @@
 
 import SwiftUI
 
-struct StartView: View {
+struct LoginView: View {
     @State private var email = ""
     @State private var password = ""
     
+    @State private var showAlert = false
+    
+    @State private var presentingMainView = false
+    
+    @State private var presentingRegView = false
     var body: some View {
-        Section {
-            
             VStack {
-                Text("Войти в Sippy")
+                Text("Sippy")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                Form {
-                    Section("Вход в Sippy") {
-                        TextField("example@mail", text: $email)
-                            .keyboardType(.emailAddress)
-                        
-                        
-                        SecureField("******", text: $password)
-                        
-                       
-                        
-                    }
-                    
-                    Section {
-                        
-                        
-                        Button("Войти") {
-                            // send request and check if valid
+                    .padding()
+            }
+        Spacer()
+            VStack {
+                Section() {
+                    Form {
+                        HStack {
+                            Text("Никнейм: ")
+                            TextField("example@mail,com", text: $email)
+                                .keyboardType(.emailAddress)
                         }
-                        .frame(maxWidth: .infinity)
-                        Button("Меня еще нет в Sippy") {
-                            // show new view
+                        
+                        HStack {
+                            Text("Пароль: ")
+                            SecureField("******", text: $password)
                         }
-                        .font(.system(size: 16, weight: .thin))
-                        .frame(maxWidth: .infinity)
-                        .accentColor(.blue)
                     }
+                    .frame(maxHeight: 150)
+                    .background(Color.white)
                     
                 }
+                Spacer()
+                
+                VStack {
+                    Button("Войти") {
+                        if email == "" || password == "" {
+                            showAlert = true
+                            
+                        }
+                        // send request and check if valid
+                        // presentingMainView.toggle()
+                        
+                    }
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text("Ошибка"), message: Text("Вы не ввели все данные!"), dismissButton: .default(Text("OK")) {
+                            showAlert = false
+                        })
+                    }
+                    
+                    .fullScreenCover(isPresented: $presentingMainView, content: {
+                        MainView()
+                    })
+                    .contentShape(Rectangle())
+                    .padding(.horizontal, 100.0)
+                    .padding(.vertical, 15.0)
+                    .background(Color.orange.opacity(0.5))
+                    .accentColor(.black)
+                    .cornerRadius(10)
+                    .frame(maxWidth: .infinity)
+                    
+                    
+                    
+                    Button("Меня еще нет в Sippy") {
+                        presentingRegView.toggle()
+                    }
+                    .font(.system(size: 16))
+                    .frame(maxWidth: .infinity)
+                    .accentColor(.blue)
+                    
+                    .sheet(isPresented: $presentingRegView, content: {
+                        RegistrationView()
+                    })
+                    
+                }
+                Spacer()
+                
                 
             }
-        }
     }
 }
 
 #Preview {
-    StartView()
+    LoginView()
 }
