2db7edbedd282a06ff88468e62c38bcbb03cd73f
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-05-18 03:31:57 +0700

    Add base profile design

--- Sippy/Sippy/Views/Main/ProfileView.swift
+++ Sippy/Sippy/Views/Main/ProfileView.swift
@@ -12,9 +12,81 @@ struct ProfileView: View {
     
     
     var body: some View {
-        Text("Hello, World!")
-        
-        
+        VStack {
+            Image("Logo_v2")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+            Rectangle()
+                .fill(Color.gray)
+                .frame(maxHeight: 3)
+            Text("Иван")
+                .font(.largeTitle)
+            
+            HStack {
+                Rectangle()
+                    .stroke(Color.gray, lineWidth: 3)
+                    .cornerRadius(3)
+                    .frame(width: 350, height: 160)
+                    .overlay(
+                        VStack {
+                            Text("Параметры")
+                                .font(.title)
+                                .foregroundColor(.black)
+                                .padding(.vertical)
+                            HStack {
+                                Text("Lorem ipsum")
+                                    .padding(.horizontal)
+                                Spacer()
+                            }
+                            HStack {
+                                Text("Lorem ipsum")
+                                    .padding(.horizontal)
+                                Spacer()
+                            }
+                            HStack {
+                                Text("Lorem ipsum")
+                                    .padding(.horizontal)
+                                Spacer()
+                            }
+                            HStack {
+                                Text("Lorem ipsum")
+                                    .padding(.horizontal)
+                                Spacer()
+                            }
+                            
+                            Spacer()
+                        }
+                        
+                    )
+            }
+            
+            HStack {
+                Rectangle()
+                    .stroke(Color.gray, lineWidth: 3)
+                    .cornerRadius(3)
+                    .frame(width: 350, height: 250)
+                    .overlay(
+                        VStack {
+                            Text("О себе")
+                                .font(.title)
+                                .foregroundColor(.black)
+                                .padding(.vertical)
+                            HStack {
+                                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+                                    .padding(.horizontal)
+                                Spacer()
+                            }
+                            
+                            Spacer()
+                        }
+                        
+                    )
+            }
+            
+        }
+        Spacer()
     }
 }
 
