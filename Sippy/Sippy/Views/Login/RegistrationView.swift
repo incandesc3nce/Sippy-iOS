3aa73786a79d5649ed34605867fac64e6e05ac2d
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-05-18 02:30:08 +0700

    Add registration API service

--- Sippy/Sippy/Views/Login/RegistrationView.swift
+++ Sippy/Sippy/Views/Login/RegistrationView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 
 struct RegistrationView: View {
     @State private var name = ""
-    @State private var gender = false
+    @State private var gender: Int = 0
+
+    @State private var ageString = ""
+    var age: Int? {
+        Int(ageString)
+    }
+    
     @State private var email = ""
     @State private var password = ""
     
     @State private var presentingMainView = false
     
+    @State private var notEnoughInfo = false
+    
+    let regService = APIServiceRegister()
+    
     var body: some View {
         Text("Регистрация")
             .font(.largeTitle)
             .fontWeight(.bold)
+            .padding()
         Section {
             
             VStack {
@@ -32,9 +43,15 @@ struct RegistrationView: View {
                         HStack {
                             Text("Пол: ")
                             Picker("", selection: $gender) {
-                                Text("Мужской").tag(false)
-                                Text("Женский").tag(true)
+                                Text("Мужской").tag(0)
+                                Text("Женский").tag(1)
                             }
+                        }
+                        HStack {
+                            Text("Ваш возраст: ")
+                            TextField("18", text: $ageString)
+                                .keyboardType(.numberPad)
+                                .multilineTextAlignment(.trailing)
                         }
                         HStack {
                             Text("Email: ")
@@ -55,9 +72,31 @@ struct RegistrationView: View {
                     
                     
                     Button("Зарегистрироваться") {
+                        if name == "" || email == "" || password == "" {
+                            notEnoughInfo.toggle()
+                        } else {
+                            regService.register(name: name, email: email, password: password, gender: gender, age: age) { result in
+                                switch result {
+                                case .success(let success):
+                                    if Bool(success)! {
+                                        print("Registration successful!")
+                                    }
+                                case .failure(let error):
+                                    print("Registration failed: \(error.localizedDescription)")
+                                }
+                            }
+                            presentingMainView.toggle()
+                        }
                         // send request and check if valid
-                        presentingMainView.toggle()
+                        
                     }
+                    .alert(isPresented: $notEnoughInfo) {
+                        Alert(title: Text("Ошибка"), message: Text("Вы не ввели все данные!"), dismissButton: .default(Text("OK")) {
+                            notEnoughInfo = false
+                        })
+                    }
+                    
+                    
                     .fullScreenCover(isPresented: $presentingMainView, content: {
                         MainView()
                     })
