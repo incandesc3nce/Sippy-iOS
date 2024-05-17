e2f5e8bc32cad48c6f5ae308ce32e3b9349746e8
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-05-17 21:32:06 +0700

    Add base login view

---
+++ Sippy/Sippy/Views/Login/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  Sippy
+//
+//  Created by Dmitry on 17.05.2024.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
