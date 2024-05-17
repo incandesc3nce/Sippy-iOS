bd31c38503386452db9cf3cd3d295a48d61900c2
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-05-17 21:33:07 +0700

    Add registration view

--- Sippy/Sippy/Views/Login/RegistrationView.swift
+++ Sippy/Sippy/Views/Login/RegistrationView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct RegistrationView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.largeTitle)
+            .fontWeight(.bold)
+        
     }
 }
 
