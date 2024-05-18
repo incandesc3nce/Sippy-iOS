df0b50f9ad2259fbdec581f2bf174e7723966037
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-05-18 09:42:52 +0700

    Add minor changes

--- Sippy/Sippy/SippyApp.swift
+++ Sippy/Sippy/SippyApp.swift
@@ -7,12 +7,15 @@
 
 import SwiftUI
 
-struct SippyApp: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@main
+struct SippyApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
     }
 }
 
-#Preview {
-    SippyApp()
-}
+
