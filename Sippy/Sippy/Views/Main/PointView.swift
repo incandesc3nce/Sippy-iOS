609800ef9fceb59125c7f9facf0d301eb4a34da4
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-05-19 10:24:40 +0700

    Add point card to see point's details

---
+++ Sippy/Sippy/Views/Main/PointView.swift
@@ -0,0 +1,92 @@
+//
+//  PointView.swift
+//  Sippy
+//
+//  Created by Dmitry on 19.05.2024.
+//
+
+import SwiftUI
+
+struct PointView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Мероприятие")
+                    .fontWeight(.thin)
+                Text("Встреча в пабе #22")
+            }
+            .padding(.top)
+            
+            Divider()
+            
+            VStack {
+                HStack {
+                    Text("Тип мероприятия")
+                        .fontWeight(.thin)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading)
+                    Spacer()
+                }
+                HStack {
+                    Text("Пабы")
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading)
+                    Spacer()
+                }
+            }
+            VStack {
+                HStack{
+                    Text("Время")
+                        .fontWeight(.thin)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading)
+                        .padding(.top, 5.0)
+                    Spacer()
+                }
+                HStack {
+                    Text("10:30 - 13:30")
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading)
+                    Spacer()
+                }
+            }
+            VStack {
+                HStack {
+                    Text("Описание")
+                        .fontWeight(.thin)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading)
+                        .padding(.top, 5.0)
+                    Spacer()
+                }
+                HStack {
+                    Text("С первомаем, дорогая, с самым ярким весенним днём")
+                        .padding(.leading)
+                    Spacer()
+                }
+            }
+            Spacer()
+            
+            VStack {
+                Button(action: {
+                    
+                }, label: {
+                    Text("Откликнуться")
+                        .fontWeight(.bold)
+                })
+                .padding(.all)
+                
+                .background(Color.orange)
+                .accentColor(.white)
+                .cornerRadius(10.0)
+            }
+            
+            Spacer()
+            
+        }
+    }
+}
+
+#Preview {
+    PointView()
+}
