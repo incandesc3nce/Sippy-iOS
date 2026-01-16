153e180a0d7473e2dbd6ab60cd06a97ddedd27c0
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2026-01-16 12:42:37 +0700

    Design profile views

---
+++ Sippy/Sippy/Views/Main/PersonProfileView.swift
@@ -0,0 +1,67 @@
+//
+//  PersonProfileView.swift
+//  Sippy
+//
+//  Created by Dmitry on 18.05.2024.
+//
+
+import SwiftUI
+
+struct PersonProfileView: View {
+    
+    @State private var personName = "Мистер Кот"
+    @State private var personAge = 20
+    @State private var personBio = "Настоящий Мистер Кот"
+    @State private var personTelegram = "@mrcat_official"
+    
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image("Logo_v2")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .padding([.top, .horizontal])
+                
+                VStack {
+                    HStack {
+                        Text("\(personName)")
+                            .font(.title)
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                    }
+                    HStack {
+                        Text("\(personAge) лет")
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                    }
+                    
+                }
+                    
+                Spacer()
+            }
+            
+            
+            VStack {
+                Form {
+                    Section("О себе") {
+                        Text("\(personBio)")
+                            .font(.callout)
+                            .multilineTextAlignment(.leading)
+                    }
+                    Section("Telegram") {
+                        Text("\(personTelegram)")
+                    }
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PersonProfileView()
+}
